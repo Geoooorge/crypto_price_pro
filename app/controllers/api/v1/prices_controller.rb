fcf13ba6a61d2bdfc0ea709c05b07770f052b85c
George Gilmer <george.gilmer@gmail.com>
2017-08-08 19:03:41 -0400

    updated price feeds, charts, and refresh

--- app/controllers/api/v1/prices_controller.rb
+++ app/controllers/api/v1/prices_controller.rb
@@ -2,4 +2,12 @@ class Api::V1::PricesController < ApplicationController
   def index
     render json: Price.last(210)
   end
+
+  def chart_prices
+    render json: Price.where(exchange: 'bitfinex').where(currency_pair: 'btcusd').or(Price.where(exchange: 'coinbase').where(currency_pair: 'btcusd')).or(Price.where(exchange: 'bitstamp').where(currency_pair: 'btcusd')).last(300)
+  end
+
+  def chart_stream
+    render json: Price.last(30)
+  end
 end
