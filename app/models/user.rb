8a2ae6938b2761839edab9e118e60f844aca5a7e
George Gilmer <george.gilmer@gmail.com>
2017-08-29 16:50:55 -0400

    destroy notifications when user cancels account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :notifications
+  has_many :notifications, dependent: :destroy
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true
